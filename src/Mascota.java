e8a02f01ef6fb5215095744ea27f890973abb9d5
Markernan <a20221747@pucp.edu.pe>
2025-04-17 02:54:13 -0500

    Agregue clase Mascota.java

---
+++ src/Mascota.java
@@ -0,0 +1,11 @@
+public class Mascota {
+    public String nombre;
+    public int anhos;
+
+    public Mascota(String nombre, int edad) {
+        this.nombre = nombre;
+        this.anhos = anhos;
+    }
+
+
+}
